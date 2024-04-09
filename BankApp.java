7f1a8682bbb5ca26b3f51d9c9c92f8258810f583
Ivan-V-programist <ivanvinokurov965@gmail.com>
2024-04-09 22:23:21 +0100

    commiting to main

---
+++ BankApp.java
@@ -0,0 +1,10 @@
+public class BankApp {
+    public static void main(String[] args){
+        Checkings check1 = new Checkings("Bobby", "123456789",1200.0);
+
+        Savings save1 = new Savings("Johny", "123456", 100.0);
+
+        //read csv and create necessary acc
+
+    }
+}
